80d5948d31e5041d176d729709c86e7ccd17063f
Sofia Filatova <sophia.tlt@gmail.com>
2018-06-02 20:33:17 +0400

    02.06

--- javascript.js
+++ javascript.js
@@ -1,6 +1,24 @@
-let money = prompt ('Ваш бюджет на месяц?');
-let name = prompt ('Название вашего магазина?');
-let time = 19;
+let money,
+	name,
+	time,
+	price
+
+function start () {
+
+	function moneyDay () {
+	money = prompt ('Ваш бюджет на месяц?');
+}
+moneyDay();
+
+	while (isNaN(money) || money == '' || money == null) {
+		money = prompt ('Ваш бюджет на месяц?');
+	}
+
+	name = prompt ('Название вашего магазина?').toUpperCase();
+	time = 19;
+}
+
+start();
 
 
 let mainList = {
@@ -8,39 +26,46 @@ let mainList = {
 	nameShop: name,
 	shopGoods: [ ],
 		employers: {
-				employers1:'Ivan',
-				employers2:'Sergey',
-				employers3:'Maria',
-				employers4:'Dmitry',
+
 		},
 	open: true,
+	discount: true
 };
 
-for (let i=0; i<5; i++) {
-	let a = prompt ('Какой тип товаров будем продавать?');
-
-	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a !='' && a.length < 50 ) {
-	console.log('Все верно!');
-	mainList.shopGoods[i] = a;
-	} else {
-		alert('Это обязательное поле! Пожалуйста,заполни его!');
-		i=a;
+function whoIsEmployers () {
+	for (let i=0; i<4; i++) {
+		let em = prompt('Как зовут Вашего сотрудника?');
+		mainList.employers[i] = em;
 	}
 }
+whoIsEmployers();
+
+function chooseGoods() {
+	for (let i=0; i<5; i++) {
+		let a = prompt ('Какой тип товаров будем продавать?');
+
+		if ((typeof(a)) === 'string' && a !== null && a !='' && a.length < 50 ) {
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+		} else {
+			alert('Это обязательное поле! Пожалуйста,заполни его!');
+			i--;
+		}
+	}
+}
+chooseGoods();
 
 /* let i=0;
 while (i<5) {
 	let a = prompt ('Какой тип товаров будем продавать?');
 
 	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a !='' && a.length < 50 ) {
-	console.log('Все верно!');
-	mainList.shopGoods[i] = a;
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+		i++
 	} else {
 		alert('Это обязательное поле! Пожалуйста,заполни его!');
-		i=a;
 	}
-
-	i++
 }
 
 let i = 0;
@@ -48,31 +73,43 @@ do {
 	let a = prompt ('Какой тип товаров будем продавать?');
 
 	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a !='' && a.length < 50 ) {
-	console.log('Все верно!');
-	mainList.shopGoods[i] = a;
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+		i++;
 	} else {
 		alert('Это обязательное поле! Пожалуйста,заполни его!');
-		i=a;
 	}
 
-	i++
 }
 while (i<5);
 */
+function workTime(time) {
+	if (time<0) {
+		console.log('Такого просто не может быть!');
+	} else if (time > 8 && time < 20 ) {
+		console.log('Время работать!');
+		} else if (time<24) {
+			console.log('Уже слишком поздно');
+		} else {
+			console.log('В сутках только 24 часа!');
 
-if (time<0) {
-	console.log('Такого просто не может быть!');
-} else if (time > 8 && time < 20 ) {
-	console.log('Время работать!');
-	} else if (time<24) {
-		console.log('Уже слишком поздно');
-	} else {
-		console.log('В сутках только 24 часа!');
+		}
+}
+workTime(18);
 
+function discountSystem () {
+	price = prompt ('Сколько стоит ' + mainList.shopGoods[0] + '?');
+	if (discount = true) {
+		console.log('Твоя цена со скидкой: ' + price * 0.8);
 	}
+	else {
+		console.log(price);
+	}
+}
+discountSystem();
+
 
 console.log('Твой магазин: '+ name);
 console.log('Твой бюджет на 1 день: '+ money/30);
-console.log('Мы продаем: ' + mainList.shopGoods[0] + ', '+ mainList.shopGoods[1] + ' и '+ mainList.shopGoods[2]);
-
-console.log();
+console.log('Мы продаем: ' + mainList.shopGoods[0] + ', '+ mainList.shopGoods[1] + ' и '+ mainList.shopGoods[2] + ',а так же ' + mainList.shopGoods[3] + ', ' + mainList.shopGoods[4]);
+console.log('Наши сотрудники: '+ '1 - ' + mainList.employers[0] + ', 2 - ' + mainList.employers[1] + ', 3 - ' + mainList.employers[2] + ', 4 - ' + mainList.employers[3]);
