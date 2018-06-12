b97aa2ae353fce92c9853df817a7d9cb1b051fe8
Sofia Filatova <sophia.tlt@gmail.com>
2018-06-12 18:22:28 +0400

    12.06.2018

--- lesson5/ПРОЕКТ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ lesson5/ПРОЕКТ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -8,13 +8,14 @@ let getInput = document.getElementById('open-btn'),
 	getIsopenValue = document.getElementsByClassName('isopen-value')[0],
 
 	getCategories = document.getElementsByClassName('goods-item'),
-	getButton = document.getElementsByTagName('button'),
+	getButtonGoodsBudget = document.getElementsByTagName('button')[1],
+	getButton = document.getElementsByTagName('button')[2],
+	getButtonEmployers = document.getElementsByTagName('button')[3],
 	getChoose = document.querySelector('#items'),
 	getTime = document.querySelector('#time'),
 	getMoney = document.querySelector('#budget'),
 	getEmployers = document.querySelectorAll('input.hire-employers-item');
 
 
-	//getEmployers[0].style.background = 'green';
 
 
