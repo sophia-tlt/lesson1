852b0da0a1cd17f9a5873d7dc78af8fad9015489
Sofia Filatova <sophia.tlt@gmail.com>
2018-06-27 12:47:58 +0400

    27.06

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded',function(){
-	let tabs = require('tabs.js');
-	let modal = require('modal.js');
-	let form = require('form.js');
-	let contact = require('contact.js');
-	let slider = require('slider.js');
-	let calc = require('calc.js');
-	let timer = require('timer.js');
+	let tabs = require('./tabs.js');
+	let modal = require('./modal.js');
+	let form = require('./form.js');
+	let contact = require('./contact.js');
+	let slider = require('./slider.js');
+	let calc = require('./calc.js');
+	let timer = require('./timer.js');
 
 tabs();
 modal();
